3ccebc0cfe3f5bc852ad580cf77a0e8f9adfb7dc
RaM <ramsalunkhe6@gmail.com>
2022-08-08 14:09:20 +0530

    assignment done

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,13 @@ const abc = require('../introduction/intro')
 const welcome = require('../logger/logger')
 const date = require('../util/helper')
 const str = require('../validator/formatter')
+const lodashModule = require('lodash')
 const router = express.Router();
+
+const month = ['january','february','march','april','may','june','july','august','septmber','octomber','november','december'];
+const oddArr = [1,3,5,7,9,11,13,17,19,23];
+const pairsOfcity = [['horror','The Shining'],['drama','Titanic'],['thriller','Shutter Island'],['fantasy','Pans Labyrinth']];
+
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
@@ -15,6 +21,11 @@ router.get('/test-me', function (req, res) {
     console.log(str.trim())
     console.log(str.lower());
     console.log(str.upper());
+    console.log(lodashModule.chunk(month,4))
+    console.log(lodashModule.tail(oddArr));
+    const result = lodashModule.union([1,2],[2,3,4],[3,4,5]);
+    console.log(result);
+    console.log(lodashModule.fromPairs(pairsOfcity));
     res.send('My First ever api!')
 });
 
