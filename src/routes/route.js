8229186645542eb2fa1a15321faa6c49be20f5ca
RaM <ramsalunkhe6@gmail.com>
2022-08-11 17:09:01 +0530

    player assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -138,5 +138,60 @@ router.get("/sol2",function(req, res){
     res.send({data: missingNum});
 })
 
+/////////////// POST REQUEST ASSIGNMENT //////////////////////////////////////////
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+
+   router.post('/players', function (req, res) {
+
+       let newPlayer = req.body
+       let newPlayerName = newPlayer.name;
+       let isNameRepeated = false;
+
+       for(let i = 0; i<players.length; i++) {
+            if(players[i].name == newPlayerName){
+            isNameRepeated = true;
+            break;
+        }
+    }
+
+    if(isNameRepeated){
+        res.send("This player is already Existed");
+    } else {
+        players.push(newPlayer)
+        res.send(players)
+    }
+    
+   })
+
 module.exports = router;
 // adding this comment for no reason
