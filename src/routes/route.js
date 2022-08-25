a2db4ea458087537eacaa6d4f7499753c6ec430a
RaM <ramsalunkhe6@gmail.com>
2022-08-25 20:07:01 +0530

    assignment 10 date ip and path

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const moment = require('moment');
 
 const UserController= require("../controllers/userController")
 const ProductController= require("../controllers/productController")
 const OrderController = require("../controllers/orderController")
 const commonMW = require ("../middlewares/commonMiddlewares")
 
-router.get("/test-me", function (req, res) {
+
+const middle = function(req,res,next) {
+    const time = moment().format("YYYY-MM-DD hh:mm")
+    const ip = req.socket.localAddress
+    const path = req.route.path
+    console.log("You are in middleware");
+    console.log(time,ip,path);
+    next()
+}
+
+router.get("/test-me",middle, function (req, res) {
     res.send("My first ever api!")
 })
-
 
 router.post("/createProduct", ProductController.createProduct)
 router.post("/createUser", commonMW.mid1,UserController.createUser)
