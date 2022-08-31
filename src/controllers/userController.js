bf155dd08b3143a32decc77c143f1d8d2b15dacc
RaM <ramsalunkhe6@gmail.com>
2022-08-31 17:55:58 +0530

    assignment 14 status code try-catch

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,87 +2,116 @@ const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
 const createUser = async function (req, res) {
-  let data = req.body;
-  let savedData = await userModel.create(data);
-  console.log(req.newAtribute);
-  res.send({ msg: savedData });
+try {
+    let data = req.body;
+    let savedData = await userModel.create(data);
+    res.status(201).send({ msg: savedData });
+}
+catch(err) {
+    res.status(500).send({error: err.message})
+}
+    
 };
 
 const loginUser = async function (req, res) {
-  let userName = req.body.emailId;
-  let password = req.body.password;
-  let user = await userModel.findOne({ emailId: userName, password: password });
-  if (!user)
-    return res.send({
-      status: false,
-      msg: "username or the password is not corerct",
-    });
-
-  let token = jwt.sign(
-    {
-      userId: user._id.toString(),
-      batch: "plutonium",
-      organisation: "FUnctionUp",
-    },
-    "functionup-plutonium"
-  );
-  res.setHeader("x-auth-token", token);
-  res.send({ status: true, msg: "logged in Successfully", data: token });
+try {
+    let userName = req.body.emailId;
+    let password = req.body.password;
+    let user = await userModel.findOne({ emailId: userName, password: password });
+    if (!user)
+      return res.status(400).send({
+        status: false,
+        msg: "username or the password is not corerct",
+      });
+  
+    let token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        batch: "plutonium",
+        organisation: "FUnctionUp",
+      },
+      "functionup-plutonium"
+    );
+    res.setHeader("x-auth-token", token);
+    res.status(200).send({ status: true, msg: "logged in Successfully", data: token });
+}
+catch(err) {
+    res.status(404).send({error: err.message})
+}
 };
 
 const getUserData = async function (req, res) {
   // here req.user attribute is inherit from middleware (function => authenticate function)
   // which indicates user from request params
-  if (req.user.isDeleted == false) {
-    let user = await userModel.findById(req.loggedInUser)
-    res.send({ status: true, data: user });
-  } else {
-    res.send("This user is deleted");
+  try {
+      if (req.user.isDeleted == false) {
+        let user = await userModel.findById(req.loggedInUser)
+        res.status(200).send({ status: true, data: user });
+      } else {
+        res.status(200).send("This user is deleted");
+      }
+  } catch (err) {
+        res.status(404).send({error: err.message})
   }
 };
 
 const updateUser = async function (req, res) {
-  if (req.user.isDeleted == false) {
-    let userData = req.body;
-    let updatedUser = await userModel.findOneAndUpdate(
-      { _id: req.loggedInUser},
-      userData,
-      { new: true }
-    );
-    res.send({ data: updatedUser });
-  } else {
-    res.send("you can't update deleted user");
-  }
+try {
+    if (req.user.isDeleted == false) {
+      let userData = req.body;
+      let updatedUser = await userModel.findOneAndUpdate(
+        { _id: req.loggedInUser},
+        userData,
+        { new: true }
+      );
+      res.status(200).send({ data: updatedUser });
+    } else {
+      res.status(200).send("you can't update deleted user");
+    }
+} 
+catch(err) {
+    res.status(404).send({error: err.message})
+}
 };
 
 const postMessage = async function (req, res) {
-  if (req.user.isDeleted == false) {
-    let message = req.body.message;
-    let updatedPosts = req.user.posts; // req.user is from middleware
-    //add the message to user's posts
-    updatedPosts.push(message);
-    let updatedUser = await userModel.findOneAndUpdate(
-      { _id: req.loggedInUser },
-      { posts: updatedPosts },
-      { new: true }
-    );
-    return res.send({ status: true, data: updatedUser });
-  } else {
-    return res.send("This user is deleted");
-  }
+try {
+    if (req.user.isDeleted == false) {
+      let message = req.body.message;
+      let updatedPosts = req.user.posts; // req.user is from middleware
+      //add the message to user's posts
+      updatedPosts.push(message);
+      let updatedUser = await userModel.findOneAndUpdate(
+        { _id: req.loggedInUser },
+        { posts: updatedPosts },
+        { new: true }
+      );
+      return res.status(201).send({ status: true, data: updatedUser });
+    } else {
+      return res.status(200).send("This user is deleted");
+    }
+}
+catch(err) {
+    res.status(404).send({error: err.message})
+}
 };
 
 const deleteUser = async function (req, res) {
-  if (req.user.isDeleted == false) {
-    const delUser = await userModel.findOneAndUpdate(
-      {_id: req.loggedInUser},
-      { isDeleted: true },
-      { new: true }
-    );
-    res.send({ msg: "user deleted", data: delUser });
-  } else {
-    return res.send("This user is already deleted");
-  }
+try {
+    if (req.user.isDeleted == false) {
+      const delUser = await userModel.findOneAndUpdate(
+        {_id: req.loggedInUser},
+        { isDeleted: true },
+        { new: true }
+      );
+      res.status(200).send({ msg: "user deleted", data: delUser });
+    } else {
+      return res.status(200).send("This user is already deleted");
+    }
+}
+catch(err) {
+    res.status(404).send({error: err.message})
+}
 };
 
 module.exports.createUser = createUser;
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,35 +3,43 @@ const userModel = require("../models/userModel")
 
 const authenticate = async function(req, res, next) {
   //check the token in request header
-  let token = req.headers['x-auth-token'];
-  if (!token) return res.send("Token must be present in headers");
-
-  //verify token
-  let decodeToken = jwt.verify(token, "functionup-plutonium");
-  if (!decodeToken) return res.send("Invalid Token");
-  req.decodeToken = decodeToken;
-
-  //verify user exists or not
-  let userId = req.params.userId;
-  let user = await userModel.findById(userId);
-  req.user = user;
-  if (!user) {
-    return res.send("No such user exists");
+  try {
+    let token = req.headers['x-auth-token'];
+    if (!token) return res.status(401).send("Token must be present in headers");
+  
+    //verify token
+    let decodeToken = jwt.verify(token, "functionup-plutonium");
+    if (!decodeToken) return res.send("Invalid Token");
+    req.decodeToken = decodeToken;
+  
+    //verify user exists or not
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId);
+    req.user = user;
+    if (!user) {
+      return res.status(404).send("No such user exists");
+    }
+      next()
   }
-
-    next()
-}
+  catch(err) {
+    res.status(500).send({error: err.message})
+  }
+  }
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
-    let loggedInUser = req.decodeToken.userId;
-    let userToBeModified = req.params.userId;
-    req.loggedInUser = loggedInUser;
-  
-    if (loggedInUser != userToBeModified)
-      return res.send("User logged is not allowed to modify the requested users data");
-  
-    next()
+    try {
+      let loggedInUser = req.decodeToken.userId;
+      let userToBeModified = req.params.userId;
+      req.loggedInUser = loggedInUser;
+    
+      if (loggedInUser != userToBeModified)
+        return res.status(403).send("User logged is not allowed to modify the requested users data");
+      next()
+    }
+    catch(err) {
+      res.status(500).send({msg: "server error"})
+    }
 }
 
 module.exports.authenticate = authenticate
